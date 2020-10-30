2274af44df7d7141d5a9bb60b11d36dc4563b986
Matt <lezgomatt@gmail.com>
2020-10-30 23:51:18 +0800

    cleanup: Minor script cleaning

--- script.js
+++ script.js
@@ -1,141 +1,137 @@
-var configuration = [null, 'left', 'left', 'left'];
-var inProgress = false;
-var position = 'h0 v0';
-var toFlip = [];
-var buttons = [];
-var ball = document.getElementById('ball');
+const L = 'left';
+const R = 'right';
 
-function flipper(n) {
-  return function() {
-    if(inProgress) return;
+let config = [null, L, L, L];
+let inProgress = false;
+let position = 'h0 v0';
+let toFlip = [];
+let buttons = [];
+let ball = document.getElementById('ball');
 
-    if(configuration[n] == 'left')
-      configuration[n] = 'right'
-    else /* configuration[n] == 'right' */
-      configuration[n] = 'left';
-    var el = document.getElementById('x' + n);
-    el.className = 'paddle ' + configuration[n];
+let dropButtonA = document.getElementById('a');
+dropButtonA.addEventListener('click', () => { drop('a') });
+buttons.push(dropButtonA);
+
+let dropButtonB = document.getElementById('b');
+dropButtonB.addEventListener('click', () => { drop('b') });
+buttons.push(dropButtonB);
+
+for (let n = 1; n <= 3; n++) {
+  let flipButton = document.getElementById('flip' + n);
+  flipButton.addEventListener('click', () => { flipPaddle(n) });
+  buttons.push(flipButton);
+}
+
+ball.addEventListener('transitionend', run, true);
+
+for (let n = 1; n <= 3; n++) {
+  let paddle = document.getElementById('x' + n);
+  paddle.addEventListener('transitionend', updateConfig);
+}
+
+function drop(slot) {
+  if (inProgress) {
+    return;
   }
-}
-for(var i = 1; i <= 3; i++) {
-  var el = document.getElementById('flip' + i);
-  el.addEventListener('click', flipper(i));
-  buttons.push(el);
-}
 
-var el;
-el = document.getElementById('a');
-el.addEventListener('click', function() { drop('a') });
-buttons.push(el);
-el = document.getElementById('b');
-el.addEventListener('click', function() { drop('b') });
-buttons.push(el);
-
-function drop(letter) {
-  if(inProgress) return;
   inProgress = true;
-  disableButtons()
-  if(letter === 'a') position = 'h1 v2';
-  if(letter === 'b') position = 'h1 v4';
-  ball.className = "instant " + position;
-  magic({propertyName: 'top'});
+  disableButtons();
+
+  position = (slot === 'a') ? 'h1 v2' : 'h1 v4';
+
+  ball.className = 'instant ' + position;
+  run();
 }
 
-function go(n, l, r) {
-  if(configuration[n] === 'left')
-    position = l;
-  else /* configuration[n] === 'right' */
-    position = r;
-  toFlip.push(n);
+function flipPaddle(n) {
+  if (inProgress) {
+    return;
+  }
+
+  config[n] = flip(config[n]);
+  let paddle = document.getElementById('x' + n);
+  paddle.className = 'paddle ' + config[n];
 }
 
-for(var i = 1; i <= 3; i++) {
-  var el = document.getElementById('x' + i);
-  el.addEventListener('transitionend', updateConfig);
-  el.addEventListener('webkitTransitionEnd', updateConfig);
+function go(paddleNum, leftPos, rightPos) {
+  position = (config[paddleNum] === L) ? leftPos : rightPos;
+  toFlip.push(paddleNum);
 }
 
 function updateConfig() {
-  for(var i = 1; i <= 3; i++) {
-    var el = document.getElementById('x' + i);
-    configuration[i] = el.className.split(' ')[1];
+  for (let n = 1; n <= 3; n++) {
+    let paddle = document.getElementById('x' + n);
+    config[n] = paddle.className.split(' ')[1];
   }
 }
 
 function flip(dir) {
-  if(dir === 'left') return 'right';
-  if(dir === 'right') return 'left';
+  return dir === L ? R : L;
 }
 
 function flipAway() {
-  var n;
-  while(n = toFlip.pop()) {
-    var el = document.getElementById('x' + n);
-    el.className = 'paddle ' + flip(configuration[n]);
+  for (let n = toFlip.pop(); n != null; n = toFlip.pop()) {
+    let paddle = document.getElementById('x' + n);
+    paddle.className = 'paddle ' + flip(config[n]);
   }
 }
 
-ball.addEventListener('transitionend', magic, true);
-ball.addEventListener('webkitTransitionEnd', magic, true);
+function run(event) {
+  if (event != null && event.propertyName !== 'top') {
+    return;
+  }
 
-function magic(evt) {
-  if(evt.propertyName !== 'top') return;
   flipAway();
-  switch(position) {
-  // start
+  switch (position) {
   case 'h1 v2':
     position = 'h2 v2';
-  break;
+    break;
   case 'h1 v4':
     position = 'h2 v4';
-  break;
+    break;
 
-  // x1 x3
   case 'h2 v2':
     go(1, 'h3 v1', 'h3 v3');
-  break;
+    break;
   case 'h2 v4':
     go(3, 'h3 v3', 'h3 v5');
-  break;
+    break;
 
-  // x2 + corners
   case 'h3 v1':
     position = 'h4 v2';
-  break;
+    break;
   case 'h3 v3':
     go(2, 'h4 v2', 'h4 v4');
-  break;
+    break;
   case 'h3 v5':
     position = 'h4 v4';
-  break;
+    break;
 
-  // final fall
   case 'h4 v2':
     position = 'h5 v2';
-  break;
+    break;
   case 'h4 v4':
     position = 'h5 v4';
-  break;
+    break;
 
-  // end
   case 'h5 v2':
   case 'h5 v4':
     inProgress = false;
     enableButtons();
-    // making the ball fade?
-  break;
+    break;
   }
-  window.setTimeout(function(){ball.className = position}, 100);
+
+  window.setTimeout(() => { ball.className = position }, 100);
 }
 
 function enableButtons() {
-  for(var i = 0, len = buttons.length; i < len; i++) {
-    buttons[i].className = 'button enabled';
+  for (let button of buttons) {
+    button.className = 'button enabled';
   }
 }
 
 function disableButtons() {
-  for(var i = 0, len = buttons.length; i < len; i++) {
-    buttons[i].className = 'button disabled';
+  for (let button of buttons) {
+    button.className = 'button disabled';
   }
 }
